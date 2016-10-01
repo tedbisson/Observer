2ab8ef8db22dacfbbac433616e4539bc2851d4b7
Theodore Bisson <ruxpin32@users.noreply.github.com>
2016-09-30 23:03:09 -0700

    Initial Commit

---
+++ Observer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Observer
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			using (TrayIcon trayIcon = new TrayIcon())
+			{
+				// Show the system tray icon.
+				trayIcon.Display();
+
+				// Run the main loop.
+				Application.Run();
+			}
+		}
+	}
+}
---
+++ Observer/TrayIcon.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Observer
+{
+	public class TrayIcon : IDisposable
+	{
+		private NotifyIcon m_notifyIcon;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public TrayIcon()
+		{
+			m_notifyIcon = new NotifyIcon();
+		}
+
+		/// <summary>
+		/// Cleans up when the program shuts down.
+		/// </summary>
+		public void Dispose()
+		{
+			// Clean up the tray icon now!
+			m_notifyIcon.Dispose();
+		}
+
+		/// <summary>
+		/// Sets up the tray icon and starts the program.
+		/// </summary>
+		public void Display()
+		{
+			// Setup the notify icon control.
+			m_notifyIcon.MouseClick += new MouseEventHandler(OnMouseClick);
+			m_notifyIcon.Icon        = Properties.Resources.TrayIcon;
+			m_notifyIcon.Text        = "Daddy Observation Tool";
+			m_notifyIcon.Visible     = true;
+
+			// Setup the context menu.
+			m_notifyIcon.ContextMenuStrip = SetupContextMenu();
+		}
+
+		/// <summary>
+		/// Creates the context menu for the tray icon.
+		/// </summary>
+		/// <returns></returns>
+		private ContextMenuStrip SetupContextMenu()
+		{
+			ContextMenuStrip menu = new ContextMenuStrip();
+
+			ToolStripMenuItem exitItem = new ToolStripMenuItem();
+			exitItem.Text = "Exit";
+			exitItem.Click += new System.EventHandler(OnExit);
+			//exitItem.Image = Properties.Resources.ExitIcon;
+			menu.Items.Add(exitItem);
+
+			return menu;
+		}
+
+		/// <summary>
+		/// Handles the MouseClick event from the NotifyIcon control.
+		/// </summary>
+		void OnMouseClick(
+			object sender,
+			MouseEventArgs e)
+		{
+
+		}
+
+		/// <summary>
+		/// Handles the Exit menu option.
+		/// </summary>
+		void OnExit(
+			object sender,
+			EventArgs e)
+		{
+			Application.Exit();
+		}
+	}
+}
