e77342475d696c95f2f4b1bdb0660595b24682dd
Theodore Bisson <ruxpin32@users.noreply.github.com>
2016-10-07 23:22:24 -0700

    Initial clock screen behavior.

--- Observer/FormClock.Designer.cs
+++ Observer/FormClock.Designer.cs
@@ -28,19 +28,54 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.c_time = new System.Windows.Forms.Label();
+			this.c_request = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// c_time
+			// 
+			this.c_time.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.c_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.c_time.Location = new System.Drawing.Point(0, 0);
+			this.c_time.Name = "c_time";
+			this.c_time.Size = new System.Drawing.Size(244, 121);
+			this.c_time.TabIndex = 0;
+			this.c_time.Text = "4:58";
+			this.c_time.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// c_request
+			// 
+			this.c_request.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.c_request.Location = new System.Drawing.Point(12, 85);
+			this.c_request.Name = "c_request";
+			this.c_request.Size = new System.Drawing.Size(220, 23);
+			this.c_request.TabIndex = 1;
+			this.c_request.Text = "Request More Time Now";
+			this.c_request.UseVisualStyleBackColor = true;
+			this.c_request.Click += new System.EventHandler(this.c_request_Click);
 			// 
 			// FormClock
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(500, 255);
+			this.ClientSize = new System.Drawing.Size(244, 121);
+			this.Controls.Add(this.c_request);
+			this.Controls.Add(this.c_time);
+			this.MaximizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(260, 160);
 			this.Name = "FormClock";
-			this.Text = "FormClock";
+			this.ShowIcon = false;
+			this.Text = "Time Remaining";
+			this.TopMost = true;
+			this.Load += new System.EventHandler(this.FormClock_Load);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label c_time;
+		private System.Windows.Forms.Button c_request;
 	}
 }
--- Observer/FormClock.cs
+++ Observer/FormClock.cs
@@ -12,9 +12,53 @@ namespace Observer
 {
 	public partial class FormClock : Form
 	{
+		private System.Windows.Forms.Timer m_timer;
+
+		public int Time = 100;
+
 		public FormClock()
 		{
 			InitializeComponent();
+		}
+
+		private void c_request_Click(
+			object sender,
+			EventArgs e)
+		{
+			FormPassword dlg = new FormPassword();
+			if (dlg.ShowDialog() == DialogResult.OK)
+			{
+				MessageBox.Show("Ok, you get more time!");
+				Time = 100;
+			}
+		}
+
+		private void FormClock_Load(
+			object sender,
+			EventArgs e)
+		{
+			// Place the window in the bottom right corner by default.
+			int x = Screen.PrimaryScreen.WorkingArea.X + Screen.PrimaryScreen.WorkingArea.Width - Size.Width;
+			int y = Screen.PrimaryScreen.WorkingArea.Y + Screen.PrimaryScreen.WorkingArea.Height - Size.Height;
+			Location = new Point(x, y);
+
+			// Update immediately.
+			OnTimer(this, EventArgs.Empty);
+
+			// Start a timer to update the time remaining.
+			m_timer = new Timer();
+			m_timer.Tick += new EventHandler(OnTimer);
+			m_timer.Interval = 1000;
+			m_timer.Start();
+		}
+
+		private void OnTimer(
+			object sender,
+			EventArgs e)
+		{
+			// Format the minutes remaining as hours and minutes.
+			c_time.Text = String.Format("{0}:{1:0#}", (Time / 60), (Time % 60));
+			--Time;
 		}
 	}
 }
